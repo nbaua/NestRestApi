ed6f114bfa303d9f7060b7e75848840127bd2f1b
Nhilesh <66257640+Nhilesh@users.noreply.github.com>
2020-07-14 11:05:05 +0530

    Minor changes - refactoring structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { FlightsService } from './flights/flights.service';
+import { FlightsController } from './flights/flights.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [ FlightsController],
+  providers: [ FlightsService],
 })
 export class AppModule {}
